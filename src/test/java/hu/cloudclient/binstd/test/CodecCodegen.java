331ed3c93097944a72820db7999bad26ad004625
0x06 <56885523+0x000006@users.noreply.github.com>
2025-08-18 22:20:08 +0200

    feat: improve CodecCodegen

--- src/test/java/hu/cloudclient/binstd/test/CodecCodegen.java
+++ src/test/java/hu/cloudclient/binstd/test/CodecCodegen.java
@@ -14,11 +14,11 @@ public class CodecCodegen {
     }
 
     public static String getImports(int n) {
-        return getRepeated(n, "\n", "import hu.cloudclient.binstd.function.Function%d;");
+        return "import java.util.function.Function;\nimport java.util.function.BiFunction;\n" + getRepeated(3, n, "\n", "import hu.cloudclient.binstd.function.Function%d;");
     }
 
     public static String getMethodDef(int n) {
-        return "// This is an automatically generated method, you should probably not modify this\nstatic <CT, " + getRepeated(n, ", ", "T%d") +
+        return "// This is an automatically generated method, you should probably not modify this\nstatic <CT, " + getRepeated(1, n, ", ", "T%d") +
             "> Codec<CT> composite("+
             switch (n) {
                 case 1 -> "Function";
@@ -26,23 +26,23 @@ public class CodecCodegen {
                 default -> "Function" + n;
             } +
             "<" +
-            getRepeated(n, ", ", "T%d") +
+            getRepeated(1, n, ", ", "T%d") +
             ", CT> factory, " +
-            getRepeated(n, ", ", "Codec<T%1$d> c%1$d, Function<CT, T%1$d> g%1$d") +
+            getRepeated(1, n, ", ", "Codec<T%1$d> c%1$d, Function<CT, T%1$d> g%1$d") +
             ") {\n    return new Codec<>() {\n\n        @Override\n        public CT decode(DataInputWrapper in) throws IOException {\n            " +
-            getRepeated(n, "\n            ", "T%1$d v%1$d = c%1$d.decode(in);") +
+            getRepeated(1, n, "\n            ", "T%1$d v%1$d = c%1$d.decode(in);") +
             "\n            return factory.apply(" +
-            getRepeated(n, ", ", "v%1$d") +
+            getRepeated(1, n, ", ", "v%1$d") +
             ");\n        }\n\n        @Override\n        public void encode(DataOutputWrapper out, CT value) throws IOException {\n            " +
-            getRepeated(n, "\n            ", "c%1$d.encode(out, g%1$d.apply(value));") +
+            getRepeated(1, n, "\n            ", "c%1$d.encode(out, g%1$d.apply(value));") +
             "\n        }\n    };\n}";
     }
 
-    public static String getRepeated(int n, String separator, String string) {
+    public static String getRepeated(int from, int to, String separator, String string) {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
-            boolean isLast = i == n - 1;
+        for (int i = from - 1; i < to; i++) {
+            boolean isLast = i == to - 1;
 
             sb.append(string.formatted(i + 1));
 
